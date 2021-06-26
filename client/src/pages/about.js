6c360e1b5f996b32aedbe2473bcad3c8ad60690e
ldylicia <fharrisfoster@gmail.com>
2021-06-26 16:40:52 -0700

    added card profiles

---
+++ client/src/pages/about.js
@@ -0,0 +1,16 @@
+import Profilecard from "../components/Profilecard";
+
+const profiles=[
+    {
+        "Name":"Ian",
+        "subtitle":"Export Coder"
+    }
+]
+
+export default function aboutpage() {
+    return (
+        <div>
+           <Profilecard /> 
+        </div>
+    )
+}
