8562fffae6dd4998ae6cebf8d3d7461d3d5850ab
Tela Caul <telacaul@gmail.com>
2021-06-30 10:29:48 -0700

    resized nav logo/ added modal, signup and social components (ignore txt files, they are temporary)

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,7 +6,7 @@ export default function Nav() {
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="/">BrewMap
-                    <img src={cheers} width="112" height="28"></img>
+                    <img src={cheers} width="28" height="28"></img>
                 </a>
 
                 <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
