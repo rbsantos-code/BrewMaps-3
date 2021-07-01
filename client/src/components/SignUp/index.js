091f506799c4ce15ee623e8f60d0c74f56059edd
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-07-01 13:51:54 -0700

    add user functionality

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import Auth from '../../utils/auth';
+import { ADD_USER } from '../../utils/mutations';
 
-export default function SignUp() {
+export default function SignUp(props) {
+
+    const [formState, setFormState] = useState({ username: '', password: ''});
+
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        const mutationResponse = await addUser({
+            variables: {
+                username: formState.username,
+                password: formState.password,
+            },
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+
     return (
         <section class="hero is-fullheight">
             <div class="hero-body">
@@ -9,16 +39,20 @@ export default function SignUp() {
                         <div class="column is-5-tablet is-4-desktop is-3-widescreen">
                             <div class="field bg-dark text-light p-3">
                                 <h3 class="text-center is-size-3 has-text-centered">Join the Brew Crew!</h3>
-                                <form class="box">
+                                <form class="box" onSubmit={handleFormSubmit}>
                                     <div className="field">
                                         <label for="username-signup" >Username:</label>
                                         <br />
-                                        <input class="portfolio-languages text-dark input is-info" type="text" id="username-signup" />
+                                        <input class="portfolio-languages text-dark input is-info" type="text" id="username-signup"
+                                        name="username"
+                                        onChange={handleChange} />
                                     </div>
                                     <div className="field">
                                         <label for="password-signup">Password:</label>
                                         <br />
-                                        <input class="portfolio-languages text-dark input is-info" type="text" id="password-signup" />
+                                        <input class="portfolio-languages text-dark input is-info" type="text" id="password-signup"
+                                        name="password"
+                                        onChange={handleChange} />
                                     </div>
                                     <br />
                                     <div class="field">
