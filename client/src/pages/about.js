2532bab073d0f221529486e92181ee734a7606af
ldylicia <fharrisfoster@gmail.com>
2021-06-30 20:15:10 -0700

    added images to profile cards

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,16 +1,59 @@
 import Profilecard from "../components/Profilecard";
 
-const profiles=[
+const profiles = [
     {
-        "Name":"Ian",
-        "subtitle":"Export Coder"
+        "name": "Ian",
+        "subtitle": "Expert Coder",
+        "description": "Ian likes golf, SF sports, and pastrami sandwiches. He is an expert coder.",
+        "github": "https://riosborne6.github.io/Portfolio/",
+        "email": "mailto:riosborne6@gmail.com",
+        "image": "../../public/images/Ian.png",
+        "id": "card-1 card-object card-object-hf"
+    },
+    {
+        "name": "Tela",
+        "subtitle": "The Educator",
+        "description": "",
+        "github": "",
+        "email": "",
+        "image": "../../public/images/Tela.png",
+        "id": "card-2 card-object card-object-hf"
+    },
+    {
+        "name": "Rich",
+        "subtitle": "Expert Coder",
+        "description": "",
+        "github": "",
+        "email": "",
+        "image": "../../public/images/Rich.png",
+        "id": "card-3 card-object card-object-hf"
+    },
+    {
+        "name": "Felicia",
+        "subtitle": "The Professor",
+        "description": "",
+        "github": "",
+        "email": "",
+        "image": "../../public/images/Felicia Harris Foster.jpg",
+        "id": "card-4 card-object card-object-hf"
+    },
+    {
+        "name": "Jennifer",
+        "subtitle": "Expert Coder",
+        "description": "",
+        "github": "",
+        "email": "",
+        "image": "../../public/images/Ian.png",
+        "id": "card-5 card-object card-object-hf"
     }
 ]
 
 export default function aboutpage() {
     return (
         <div>
-           <Profilecard /> 
+            {profiles.map(profile => (
+                <Profilecard profile={profile} />
+            ))}
         </div>
     )
 }
