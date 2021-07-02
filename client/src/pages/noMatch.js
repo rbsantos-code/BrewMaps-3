922ad34ab1999341580f5b1854b95938aa515123
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-07-01 18:15:56 -0700

    styled noMatch and changed route capitalization

--- client/src/pages/noMatch.js
+++ client/src/pages/noMatch.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const NoMatch = () => {
-    return (
-        <>
-            <section className="hero is-full height">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="columns is-centered">
-                            <div className="column is-5-tablet is-4-desktop is-3-widescreen">
-                                <div className="field">
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <section className="hero is-full height">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                <div className="field">
+                  <h1 className="title text-light" style= {{ fontheight: 560, clear: "both", paddingTop: 120, textAlign: "center" }}> 404 Error: No Brews Here! <span role="img" aria-label="Beers">
+                    🚫🍻
+                    </span></h1>
                 </div>
-            </section>
-        </>
-    )
-}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default NoMatch;
