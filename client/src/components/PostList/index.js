4a18f4cbac40f9197214b093134863e7c82cde54
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-07-03 18:24:31 -0700

    show posts

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PostList = ({ posts, body }) => {
+const PostList = ({ posts }) => {
     if (!posts.length) {
         return <h2>No post yet! Be the first to do so!</h2>
     }
 
     return (
-        <div class="media">
-            <figure class="media-left">
-                <p class="image is-64x64">
-                    <img src="https://bulma.io/images/placeholders/128x128.png"></img>
-                </p>
-            </figure>
-            <div class="media-content">
-                <p>
-                    <Link to={`/profile/${post.username}`}>
-                    <strong>{post.username}</strong>
-                    </Link>
-                    <br />
-                    <Link to={`/post/${post._id}`}>
-                        {post.body}
-                    </Link>
-                    <br />
-                    <small><a>Like</a> . <a>Reply</a> . 3hrs</small>
-                </p>
-            </div>
-        </div>
+        <>
+           <article className="column">
+           {posts && 
+                posts.map(posts => (
+                <article className="media">
+                    <figure class="media-left">
+                        <p class="image is-64x64">
+                            <img src="https://bulma.io/images/placeholders/128x128.png"></img>
+                        </p>
+                    </figure>
+                    <div class="media-content">
+                        <div className="content">
+                            <p>
+                                <Link to={`/profile/${posts.username}`}>
+                                <strong>@{posts.username}</strong>
+                                </Link>
+                                <br />
+                                <Link to={`/post/${posts._id}`}>
+                                    {posts.body}
+                                </Link>
+                                <br />
+                                <small><a>Like</a> . <a>Reply</a> . 3hrs</small>
+                            </p>
+                        </div>
+                    </div>
+                </article>
+            ))}
+           </article>
+        </>
     )
 }
 
